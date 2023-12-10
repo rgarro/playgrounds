eb04fcc35e613e8d743265c917fea2184b31c39f
rgarro <rgarro@gmail.com>
2023-12-10 11:17:05 -0600

    android is your own virus ..

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var greeting = "Hello, playground"
+greeting = "Another String"
+print(greeting)
+var str = String(1)
+var str2: Int = 1
+var j = 0
+while str2 < 100 {
+ str2 += 1
+   // print(str2)
+}
+print(str2)
